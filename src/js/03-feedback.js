498af6d39ae95ecfe716c66dfae2d39c998cb8d2
Enma04 <programmer1ebg@gmail.com>
2023-06-12 14:38:41 -0500

    Punto 3 de la tarea

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+const form = document.querySelector(".feedback-form");
 
+
+//------------------------------------------------------------------------
+// --------------- ******* --- FUNCIONES --- ******* ---------------------
+
+function almacenar(event) {
+	const {
+		elements: { email, message }
+	} = event.currentTarget;
+	//console.log(`Email: ${email.value}, Texto: ${message.value}`);
+	localStorage.setItem("feedback-form-state", `{"email": "${email.value}", "message": "${message.value}"}`);
+}
+
+function validarSubmit() {
+	const datos = JSON.parse(localStorage.getItem("feedback-form-state"));
+	if (datos.email === "" || datos.message === "") {
+		return alert("Por favor llene todos los espacios!");
+	}
+	//localStorage.setItem("feedback-form-state", `{"email": "", "message": ""}`);
+	alert(`Usted envió: \nEmail = ${datos.email} \nMensaje = ${datos.message}`);
+	localStorage.removeItem("feedback-form-state");
+}
+
+function cargarDatos() {
+	const datos = JSON.parse(localStorage.getItem("feedback-form-state"));
+	form.email.value = datos.email;
+	form.message.value = datos.message;
+}
+
+
+//------------------------------------------------------------------------
+// --------------- ******* --- IMPLEMENTS --- ******* ---------------------
+
+document.addEventListener("DOMContentLoaded", cargarDatos);
+form.addEventListener("input", almacenar);
+form.addEventListener("submit", validarSubmit);
